6aefbf1f563ed2a4b759ec5286c691a22b0cc026
Junhyeong Ahn <rkdrnf@gmail.com>
2021-08-25 20:23:15 +0900

    fixed datestring

--- index.js
+++ index.js
@@ -52,6 +52,34 @@ const buildMessage = function ({ title, env, version, date, actor, link }) {
     icon_emoji: ":hatching_chick",
   };
 };
+
+function toIsoString(date) {
+  const tzo = 9;
+  (dif = tzo >= 0 ? "+" : "-"),
+    (pad = function (num) {
+      var norm = Math.floor(Math.abs(num));
+      return (norm < 10 ? "0" : "") + norm;
+    });
+
+  return (
+    date.getFullYear() +
+    "-" +
+    pad(date.getMonth() + 1) +
+    "-" +
+    pad(date.getDate()) +
+    "T" +
+    pad(date.getHours()) +
+    ":" +
+    pad(date.getMinutes()) +
+    ":" +
+    pad(date.getSeconds()) +
+    dif +
+    pad(tzo / 60) +
+    ":" +
+    pad(tzo % 60)
+  );
+}
+
 async function run() {
   try {
     const webhookUrl = core.getInput("webhookUrl");
@@ -65,7 +93,7 @@ async function run() {
           title: core.getInput("title"),
           env: core.getInput("env"),
           version: core.getInput("version"),
-          date: new Date().toTimeString(),
+          date: toIsoString(new Date()),
           actor: context.actor,
           link: core.getInput("link"),
         })
